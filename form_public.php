ed98c4266b0db57ecfc2685b2ec4db70c827f4c2
luisenrique1234 <zazazazzazzaz8@gmail.com>
2021-09-25 22:51:13 -0400

    trabajando con los fromularios de publicacin

--- form_public.php
+++ form_public.php
@@ -17,7 +17,7 @@ $codi= mb_detect_encoding($str,"ISO-8859-1,UTF-8");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Red social medica</title>
+    <title>Publicar</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/lightbox.css" rel="stylesheet"> 
@@ -133,7 +133,7 @@ $codi= mb_detect_encoding($str,"ISO-8859-1,UTF-8");
                 <div class="row">
                     <div>
                         <div class="col-sm-12">
-                            <h1 class="title">Inicio</h1>
+                            <h1 class="title">Publicar</h1>
                             <p></p>
                         </div>
                     </div>
@@ -193,113 +193,125 @@ $codi= mb_detect_encoding($str,"ISO-8859-1,UTF-8");
                                 <li><a href="#">Radiología <span class="pull-right">(8)</span></a></li>
                             </ul>
                         </div>
-                        <div class="sidebar-item tag-cloud">
-                            <h3>Etiquetas</h3>
-                            <ul class="nav nav-pills">
-                                <li><a href="#">Corporate</a></li>
-                                <li><a href="#">Joomla</a></li>
-                                <li><a href="#">Abstract</a></li>
-                                <li><a href="#">Creative</a></li>
-                                <li><a href="#">Business</a></li>
-                                <li><a href="#">Product</a></li>
-                            </ul>
-                        </div>
-                        <div class="sidebar-item popular">
-                            <h3>Latest Photos</h3>
-                            <ul class="gallery">
-                                <li><a href="#"><img src="images/portfolio/popular1.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="images/portfolio/popular2.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="images/portfolio/popular3.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="images/portfolio/popular4.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="images/portfolio/popular5.jpg" alt=""></a></li>
-                                <li><a href="#"><img src="images/portfolio/popular1.jpg" alt=""></a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
                 <div class="col-md-9 col-sm-7">
-                <?php
-					include 'php/conexion.php';
-                    $public="SELECT publicacion.titulo_public,publicacion.text_public,publicacion.link_imagen,publicacion.link_video,
-                    publicacion.link_audio,publicacion.link_archivo,publicacion.fecha_public,publicacion.categoria_public,publicacion.me_gusta_pu,
-                    medico.nombre_medico FROM publicacion,medico WHERE publicacion.id_medico_pu=medico.id_medico ORDER BY publicacion.me_gusta_pu DESC";
-					$public2 = $mysqli->query ($public);
-                    while($res= mysqli_fetch_array($public2)){
-                        $link_imagen=$res['link_imagen'];
-                        $video=$res['link_video'];
-                        $audio=$res['link_audio'];
-                        $fecha=$res['fecha_public'];
-                        $archivo=$res['link_archivo'];
-                        ?>
-                        <!--animacion js wow fadeInDowm de las publicaciones-->
-                    <div class="wow fadeInDown">
+              
+
                     <div class="row">
                             <div class="col-md-12 col-sm-12">
                             <div class="single-blog two-column">
                                 <div class="post-thumb">
-                                    <?php
-                                    if ($link_imagen !='') {
-                                    
-                                    echo ('<a href="blogdetails.html"><img src="'.$link_imagen.'" class="img-responsive" alt="">');}?></a>
-                                    <?php
-                                    if ($video !='') {
-                                        echo('<video width="820" height="420"  controls src="'.$video.'" frameborder="0"></video>');
-                                    }?>
-                                    
-                                    
-                                    <div class="post-overlay">
-                                        <span class="uppercase"><a href="#"><h4><?php mostrar($res['fecha_public']); ?></h4></a></span>
-                                    </div>
-                                    
-                                </div>
-                                <?php
-                                    if ($audio !='') {
-                                        echo ('<audio src="'.$audio.'" preload="none" controls></audio>');
-                                        echo ("<h4> $fecha </h4>");
-                                        }?>
-                                        <?php
-                                    if ($archivo!='') {
-                                        echo ("<h4> $fecha </h4>");
-                                        echo ('<h4 class="post-author"><a href="'.$archivo.'"download="red-medica">Descargar Archivo</a></h4>');
-                                        
-                                        }?>
-                                <div class="post-content overflow">
-                                    <h2><?php mostrar($res ['titulo_public']);?></h2>
-                                    <h3 class="post-author"><a href="#">Publicado por: <?php mostrar($res ['nombre_medico']);?> </a></h3>
-                                    <p><?php mostrar($res ['text_public']);?></p>
-                                    <a href="#" class="read-more">ver publica completa</a>
-                                    <div class="post-bottom overflow">
-                                        <ul class="nav navbar-nav post-nav">
-                                            <li><h4><a href="#"><i class="fa fa-tag"></i><?php mostrar($res['categoria_public']);?></a></h4></li>
-                                            <li><h4><a href="#"><i class="fa fa-heart"></i><?php mostrar($res['me_gusta_pu']); ?> me gusta</a></h4></li>
-                                            <li><h4><a href="#"><i class="fa fa-comments"></i>3 comentarios</a></h4></li>
-                                        </ul>
-                                    </div>
+                                <div class="panel-dafault" style="margin-top: 12px" > <!--panel de crear -->
+         <div class="panel-heading">  
+  	   </div>
+  	     <div class="panel-body wow fadeInDown">
+		   <form action="php/alumno_registro.php?accion=INS" method="POST">
+  	     	 	<div class="row">
+
+					<div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-lg-2 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+  	     	 		 	<div class="form-group">
+							 <label class="control-label">Grupos<span style="color: Red">*</span></label>
+					<select name="grupos" class = "form-control">
+					<?php
+					include 'php/conexion.php';
+					$getAlumno1 = "select * from grupos ORDER BY id_grupos";
+					$gerAlumno2 = $mysqli->query ($getAlumno1);
+					while ($row = mysqli_fetch_array($gerAlumno2))
+					{
+						$id = $row ['id_grupos'];
+						$nombregru = $row['nombre_grupos'];
+						
+
+						?>
+						<option value="<?php echo $id;?>"><?php echo $nombregru;?> </option>
+
+						<?php
+
+					}
+
+					?>
+					</select>
+							 </div>
+					</div>
+
+					<div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-lg-2 col-lg-offset-2 col-xs-12 col-xs-offset-0">
+                         <div class="form-group">
+                        <label for="" class="control-label">Sexo <span style="color:red">*</span> </label>
+                        <select class="form-control" name="sexo">
+                            <option>Hombre</option>
+                            <option>Mujer</option>
+                        </select>
+                        </div>
+  	     	 		 </div>
+  	     	 	     </div>
+					</div>
+					</div>
+
+					
+
+					
+  	     	 		<div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-3 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+  	     	 		 	<div class="form-group" class="alert alert-danger" role="alert">
+							 <i class="glyphicon glyphicon-user"></i>
+  	     	 		 		<label class="control-label">Nombres<span style="color: Red">*</span></label>
+  	     	 		 		<input type="text" name="nombre" required="" placeholder="Nombres" class="form-control">
+  	     	 		 	</div>
+						 </div>
+                         </div>
+                         <div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-9 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+						 <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
+                            </div> 
+                         </div>
+  	     	 		 <!--segunda parte del formulario --> 
+  	     	 		 <div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-3 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+  	     	 		 	<div class="form-group">
+  	     	 		 		<label class="control-label">Apellidos<span style="color: Red">*</span></label>
+  	     	 		 		<input type="text" name="apellidos" required="" placeholder="Apellidos" class="form-control">
+  	     	 		 	</div>
+  	     	 		 </div>
+					</div>
+					</div>
+
+						 
+  	     	 		 <!--para cambiarlas posiciones col-lg-offset-3 --> 
+  	     	 		 <div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-1 col-lg-2 col-lg-offset-3 col-xs-10 col-xs-offset-0">
+  	     	 		 	<div class="form-group">
+							 <i class="far fa-calendar-alt"></i>
+  	     	 		 		<label class="control-label">Fecha de Nacimiento<span style="color: Red">*</span></label>
+  	     	 		 		<input type="date" name="fecha" required="" placeholder="Fecha de Nacimiento" class="form-control">
+  	     	 		 	</div>
+  	     	 		 </div>
+  	     	 	</div>
+               
+
+  	     	 	<!-- parte que ocupada la pantalla completa -->
+  	     	 	<div class="row">
+  	     	 		<div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-lg-8 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+  	     	 			<div class="form-group">
+						
+  	     	 				<a href="lista_alumnos.php" class="btn btn-danger">Atras</a>
+  	     	 				<input type="submit" value="Guardar" class="btn btn-success">
+
+								
+  	     	 			</div>
+  	     	 		</div>
+  	     	 	</div>
+
+  	     	 </form>
+  	     </div>
+  </div>
+                                   
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </div>
-            
+                </div>
                 
-                        <?php
-					}
-                    ?>
-                    <div class="blog-pagination">
-                        <ul class="pagination">
-                          <li><a href="#">left</a></li>
-                          <li><a href="#">1</a></li>
-                          <li><a href="#">2</a></li>
-                          <li class="active"><a href="#">3</a></li>
-                          <li><a href="#">4</a></li>
-                          <li><a href="#">5</a></li>
-                          <li><a href="#">6</a></li>
-                          <li><a href="#">7</a></li>
-                          <li><a href="#">8</a></li>
-                          <li><a href="#">9</a></li>
-                          <li><a href="#">right</a></li>
-                        </ul>
-                    </div>
+                
+                        
+                    
                  </div>
             </div>
         </div>
