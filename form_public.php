2a881230ae3de14026927cd7e3e37b86eed0a023
luisenrique1234 <zazazazzazzaz8@gmail.com>
2021-09-27 21:24:05 -0400

    terminado el formulario de publicion

--- form_public.php
+++ form_public.php
@@ -198,116 +198,60 @@ $codi= mb_detect_encoding($str,"ISO-8859-1,UTF-8");
                 <div class="col-md-9 col-sm-7">
               
 
-                    <div class="row">
+                    
                             <div class="col-md-12 col-sm-12">
-                            <div class="single-blog two-column">
+                            
                                 <div class="post-thumb">
                                 <div class="panel-dafault" style="margin-top: 12px" > <!--panel de crear -->
          <div class="panel-heading">  
-  	   </div>
-  	     <div class="panel-body wow fadeInDown">
+  	   
+  	     
 		   <form action="php/alumno_registro.php?accion=INS" method="POST">
   	     	 	<div class="row">
-
-					<div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-lg-2 col-lg-offset-3 col-xs-12 col-xs-offset-0">
-  	     	 		 	<div class="form-group">
-							 <label class="control-label">Grupos<span style="color: Red">*</span></label>
-					<select name="grupos" class = "form-control">
-					<?php
-					include 'php/conexion.php';
-					$getAlumno1 = "select * from grupos ORDER BY id_grupos";
-					$gerAlumno2 = $mysqli->query ($getAlumno1);
-					while ($row = mysqli_fetch_array($gerAlumno2))
-					{
-						$id = $row ['id_grupos'];
-						$nombregru = $row['nombre_grupos'];
-						
-
-						?>
-						<option value="<?php echo $id;?>"><?php echo $nombregru;?> </option>
-
-						<?php
-
-					}
-
-					?>
-					</select>
-							 </div>
-					</div>
-
-					<div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-lg-2 col-lg-offset-2 col-xs-12 col-xs-offset-0">
+                    <div class="wow fadeInDown">
+  	     	 		<div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-5 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+  	     	 		 	<div class="form-group" class="alert alert-danger" role="alert">
+  	     	 		 		<label class="control-label">Titulo<span style="color: turquoise">*</span></label>
+  	     	 		 		<input type="text" name="nombre" required="required" placeholder="Titulo" class="form-control">
+  	     	 		 	</div>
+						 </div>
+                         
+                            
+                         
+                         <div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-8 col-lg-offset-3 col-xs-12 col-xs-offset-0">
+                         <label class="control-label">Contenido<span style="color: turquoise">*</span></label>
+						 <div class="form-group">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Escribe una publicacion"></textarea>
+                            </div> 
+                         </div>
+  	     	 		 <div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-2 col-lg-3 col-lg-offset-3 col-xs-12 col-xs-offset-0">
                          <div class="form-group">
-                        <label for="" class="control-label">Sexo <span style="color:red">*</span> </label>
-                        <select class="form-control" name="sexo">
-                            <option>Hombre</option>
-                            <option>Mujer</option>
+                        <label for="" class="control-label">Categoria<span style="color:turquoise">*</span> </label>
+                        <select class="form-control" name="cate">
+                            <option>Pediatria </option>
+                            <option>Radiologia</option>
+                            <option>Cardiologia</option>
                         </select>
                         </div>
   	     	 		 </div>
-  	     	 	     </div>
-					</div>
-					</div>
-
 					
-
-					
-  	     	 		<div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-3 col-lg-offset-3 col-xs-12 col-xs-offset-0">
-  	     	 		 	<div class="form-group" class="alert alert-danger" role="alert">
-							 <i class="glyphicon glyphicon-user"></i>
-  	     	 		 		<label class="control-label">Nombres<span style="color: Red">*</span></label>
-  	     	 		 		<input type="text" name="nombre" required="" placeholder="Nombres" class="form-control">
-  	     	 		 	</div>
-						 </div>
-                         </div>
-                         <div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-9 col-lg-offset-3 col-xs-12 col-xs-offset-0">
-						 <div class="form-group">
-                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
-                            </div> 
-                         </div>
-  	     	 		 <!--segunda parte del formulario --> 
-  	     	 		 <div class="col-md-4 col-md-offset-2 col-sm-4 col-sm-offset-2 col-lg-3 col-lg-offset-3 col-xs-12 col-xs-offset-0">
-  	     	 		 	<div class="form-group">
-  	     	 		 		<label class="control-label">Apellidos<span style="color: Red">*</span></label>
-  	     	 		 		<input type="text" name="apellidos" required="" placeholder="Apellidos" class="form-control">
-  	     	 		 	</div>
-  	     	 		 </div>
-					</div>
-					</div>
-
-						 
-  	     	 		 <!--para cambiarlas posiciones col-lg-offset-3 --> 
-  	     	 		 <div class="col-md-3 col-md-offset-2 col-sm-3 col-sm-offset-1 col-lg-2 col-lg-offset-3 col-xs-10 col-xs-offset-0">
-  	     	 		 	<div class="form-group">
-							 <i class="far fa-calendar-alt"></i>
-  	     	 		 		<label class="control-label">Fecha de Nacimiento<span style="color: Red">*</span></label>
-  	     	 		 		<input type="date" name="fecha" required="" placeholder="Fecha de Nacimiento" class="form-control">
-  	     	 		 	</div>
-  	     	 		 </div>
-  	     	 	</div>
-               
-
   	     	 	<!-- parte que ocupada la pantalla completa -->
-  	     	 	<div class="row">
+  	     	 	
   	     	 		<div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-lg-8 col-lg-offset-3 col-xs-12 col-xs-offset-0">
   	     	 			<div class="form-group">
-						
-  	     	 				<a href="lista_alumnos.php" class="btn btn-danger">Atras</a>
-  	     	 				<input type="submit" value="Guardar" class="btn btn-success">
-
-								
+                                <br>
+                                <input type="submit" value="Publicar" class="btn btn-submit">
+                                <br>
+                                <br>	
+                                <a href="index.php" class="btn btn-danger">Cancelar</a>
   	     	 			</div>
   	     	 		</div>
-  	     	 	</div>
-
+  	     	 	
   	     	 </form>
   	     </div>
-  </div>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+         </div> 
+         </div>
+                                    
                 
                 
                         
@@ -323,8 +267,8 @@ $codi= mb_detect_encoding($str,"ISO-8859-1,UTF-8");
                 <div class="col-sm-12 text-center bottom-separator">
                 <div class="col-sm-12">
                     <div class=" copyright-text text-center ">
-                        <p> Your Company 2021. All Rights Reserved.</p>
-                        <p>Designed by <a target="_blank" href="http://www.themeum.com">Themeum</a></p>
+                        <p>Red medica 2021. Todos los derechos reservados.</p>
+                        <p>Diseñado por <a target="_blank" href="http://luis-enrique.com">Sr.LEGG</a></p>
                     </div>
                 </div>
             </div>
